ce5f40526b54df7282fc1cb53d5c99e6167b0649
bgomiss <aduskun@gmail.com>
2023-03-12 02:02:19 +0300

    Initial Commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by aycan duskun on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
