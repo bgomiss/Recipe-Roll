27a120a4e63e2f539bc7968d4d19267419289143
bgomiss <ditty-inning-0v@icloud.com>
2023-04-25 10:30:28 +0300

    recipeCell created

---
+++ practice/Custom Views/Cells/RecipesCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesCell.swift
+//  practice
+//
+//  Created by aycan duskun on 25.04.2023.
+//
+
+import UIKit
+
+class RecipesCell: UITableViewCell {
+
+    static let reuseID = "RecipesCell"
+}
