5510e3fc1a81e310eadbb869fea5cc774c9c690f
bgomiss <ditty-inning-0v@icloud.com>
2023-08-13 11:23:22 +0300

    findByIngredients model added

---
+++ practice/Model/FindByIngredients.swift
@@ -0,0 +1,31 @@
+//
+//  FindByIngredients.swift
+//  practice
+//
+//  Created by aycan duskun on 13.08.2023.
+//
+
+import Foundation
+
+// MARK: - RecipeSearchElement
+struct FindByIngredients: Codable {
+    let id: Int
+    let title: String
+    let image: String
+    let imageType: String
+    let usedIngredientCount, missedIngredientCount: Int
+    let missedIngredients, usedIngredients: [SedIngredient]
+    let unusedIngredients: [JSONAny]
+    let likes: Int
+}
+
+// MARK: - SedIngredient
+struct SedIngredient: Codable {
+    let id: Int
+    let amount: Double
+    let unit, unitLong, unitShort, aisle: String
+    let name, original, originalName: String
+    let meta: [String]
+    let image: String
+    let extendedName: String?
+}
