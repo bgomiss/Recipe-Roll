6c8480e1105e33b9a0b24eecca572ae0011293b1
bgomiss <ditty-inning-0v@icloud.com>
2024-02-08 22:35:27 +0300

    no message

--- practice/View/Screens/BookmarksVC.swift
+++ practice/View/Screens/BookmarksVC.swift
@@ -38,7 +38,7 @@ class BookmarksVC: UIViewController {
         collectionView.register(LunchHeaderView.self, forSupplementaryViewOfKind: "LunchHeader", withReuseIdentifier: LunchHeaderView.headerIdentifier)
         collectionView.register(DinnerHeaderView.self, forSupplementaryViewOfKind: "DinnerHeader", withReuseIdentifier: DinnerHeaderView.headerIdentifier)
         collectionView.backgroundColor = .systemBackground
-       
+        
         return collectionView
     }()
     
@@ -53,7 +53,7 @@ class BookmarksVC: UIViewController {
         configure()
         configureUIElements()
         view.backgroundColor = .systemBackground
-
+        
     }
     
     
@@ -63,11 +63,11 @@ class BookmarksVC: UIViewController {
     }
     
     
-//    @objc func handleBookmarkAddedNotification() {
-//        self.collectionView.reloadData()
-//    }
+    //    @objc func handleBookmarkAddedNotification() {
+    //        self.collectionView.reloadData()
+    //    }
     
-   
+    
     func fetchBookmarkedRecipeIDs() {
         var categoryID: String = ""
         guard let userID = Auth.auth().currentUser?.uid else { return }
@@ -85,183 +85,162 @@ class BookmarksVC: UIViewController {
                 categoryID = category.documentID
                 print("CATEGORY ID IS: \(String(describing: categoryID))")
                 
-                let recipesCollection = userBookmarkCollection.document(categoryID).collection(categoryID)
-                
-                recipesCollection.getDocuments { [weak self] querySnapshot, error in
-                    guard let self = self else { return }
+                // Access the subcollection corresponding to the current category
+//                let recipesCollection = userBookmarkCollection.document(categoryID).collection(categoryID)
+//                
+//                recipesCollection.getDocuments { [weak self] querySnapshot, error in
+//                    guard let self = self else { return }
+//                    
+//                    if let error = error {
+//                        print("Error fetching recipes: \(error)")
+//                        return
+//                    }
+//                }
+                    guard let recipes = querySnapshot?.documents else { return }
                     
-                    if let error = error {
-                        print("Error fetching recipes: \(error)")
-                        return
-                    }
-                }
-                    
-                guard let recipes = querySnapshot?.documents else { return }
-                
-                /// YOU NEED YOUR RECIPEID AND YOUR DEBUG IS NOT PASSING HERE
-                /// GUIMEL
-               
-                if categoryID == "Recently Viewed" {
-                    if let firstRecipe = recipes.first,
-                       let recipeData = firstRecipe.data() as? [String: Any],
-                       let nestedDict  = recipeData.values.first as? [String: Any],
-                       let recipeID = nestedDict["id"] as? Int {
-                        print("TarifID is: \(recipeID)")
-                        
-                        // Create a completion handler closure
-                        let completion: (Result<[GetSimilarRecipes], SPError>) -> Void = { result in
-                            switch result {
-                            case .success(let similarRecipes):
-                                print("Fetched similar recipes: \(similarRecipes)")
-                            case .failure(let error):
-                                print("Error fetching similar recipes: \(error.localizedDescription)")
-                            }
-                        }
-                        
-                        if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate,
-                           let existingTabBarController = sceneDelegate.window?.rootViewController as? SPTabBarController,
-                           let homeNavVC = existingTabBarController.viewControllers?[0] as? UINavigationController,
-                           let homeVC = homeNavVC.viewControllers.first as? HomeVC {
-                            if (recipeID != 0) {
-                                homeVC.fetchSimilarRecipes(recipeID: String(recipeID))
-                                
-                            } else {
-                                homeVC.fetchSimilarRecipes(recipeID: "651942")
-                            }
+                    if categoryID == "Recently Viewed" {
+                        if let firstRecipe = recipes.first,
+                           let recipeData = firstRecipe.data() as? [String: Any],
+                           let nestedDict  = recipeData.values.first as? [String: Any],
+                           let recipeID = nestedDict["id"] as? Int {
+                            print("TarifID is: \(recipeID)")
                             
+                            if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate,
+                               let existingTabBarController = sceneDelegate.window?.rootViewController as? SPTabBarController,
+                               let homeNavVC = existingTabBarController.viewControllers?[0] as? UINavigationController,
+                               let homeVC = homeNavVC.viewControllers.first as? HomeVC {
+                                if (recipeID != 0) {
+                                    homeVC.fetchSimilarRecipes(recipeID: String(recipeID))
+                                    
+                                } else {
+                                    homeVC.fetchSimilarRecipes(recipeID: "651942")
+                                }
+                                
+                            }
                         }
                     }
-                }
-
+                    
                     for recipe in recipes {
                         let recipeData = recipe.data()
-                        //print("recipe data: \(recipeData)")
-                        
-                        //let dispatchGroup = DispatchGroup()
-                        //var totalRecipeDetailIterations = 0
                         for (_, recipeDetail) in recipeData {
                             print("Recipe DETAIL: \(recipeDetail)")
-                            //totalRecipeDetailIterations += 1
                             if let detailDict = recipeDetail as? [String: Any],
                                let recipeID = detailDict["id"] as? Int64 {
-                                //print("RECIPEDETAIL is: \(recipeDetail)")
-                                //dispatchGroup.enter()
-                                
                                 self.getCategories(query: String(recipeID), categoryID: categoryID) {
                                     
                                 }
-                            
-                        }
-                        //dispatchGroup.notify(queue: .main) {
-                       // print("Total Iterations is: \(totalRecipeDetailIterations)")
+                                
+                            }
                         }
                     }
-                
+                    
                 }
             }
         }
     
-
-    func createDismissKeyboardTapGesture() {
-        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
-        view.addGestureRecognizer(tap)
-    }
-    
-    
-    func configure() {
-        collectionView.setUp(to: view, and: querySearchBar)
-    }
-    
-    
-    func configureUIElements() {
-        let searchIcon = UIImage(systemName: "magnifyingglass")
-        let imageView = UIImageView(image: searchIcon)
-        imageView.contentMode = .scaleAspectFit
-//        queryTextField.leftViewMode = .always
-//        queryTextField.leftView = imageView
-    }
-    
-    
-    func getCategories(query: String, categoryID: String, completion: @escaping() -> Void) {
-        NetworkManager.shared.getRecipesInfo(for: .bookmarks(query)) { [weak self] result in
-            
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let newRecipes):
-                        DispatchQueue.main.async {
-                            // Check if the categoryID exists in the array
-                            if let index = self.recipes.firstIndex(where: { $0.0 == categoryID }) {
-                                // If exists, append to existing recipes
-                                self.recipes[index].1.append(contentsOf: newRecipes)
-                            } else {
-                                // If not, add a new tuple
-                                self.recipes.append((categoryID, newRecipes))
-                            }
-                            self.collectionView.reloadData()
-                            print("Total Recipe Count is: \(self.recipes.flatMap { $0.1 }.count)")
-                            
+        
+        
+        func createDismissKeyboardTapGesture() {
+            let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+            view.addGestureRecognizer(tap)
+        }
+        
+        
+        func configure() {
+            collectionView.setUp(to: view, and: querySearchBar)
+        }
+        
+        
+        func configureUIElements() {
+            let searchIcon = UIImage(systemName: "magnifyingglass")
+            let imageView = UIImageView(image: searchIcon)
+            imageView.contentMode = .scaleAspectFit
+            //        queryTextField.leftViewMode = .always
+            //        queryTextField.leftView = imageView
+        }
+        
+        
+        func getCategories(query: String, categoryID: String, completion: @escaping() -> Void) {
+            NetworkManager.shared.getRecipesInfo(for: .bookmarks(query)) { [weak self] result in
+                
+                guard let self = self else { return }
+                
+                switch result {
+                case .success(let newRecipes):
+                    DispatchQueue.main.async {
+                        // Check if the categoryID exists in the array
+                        if let index = self.recipes.firstIndex(where: { $0.0 == categoryID }) {
+                            // If exists, append to existing recipes
+                            self.recipes[index].1.append(contentsOf: newRecipes)
+                        } else {
+                            // If not, add a new tuple
+                            self.recipes.append((categoryID, newRecipes))
                         }
-
-                    case .failure(let error):
-                        print(error.localizedDescription)
+                        self.collectionView.reloadData()
+                        print("Total Recipe Count is: \(self.recipes.flatMap { $0.1 }.count)")
+                        
                     }
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
                 }
-                completion()
             }
-    
-    
-//    func updateUI(with categories: [Recipe]) {
-//        recipes.append(contentsOf: categories)
-//        
-//        DispatchQueue.main.async {
-//            self.collectionView.reloadData()
-//            //self.view.bringSubviewToFront(self.tableView)
-//        }
-//    }
-    
-    
-    func layoutUI() {
-        querySearchBar.delegate = self
-    
-        NSLayoutConstraint.activate([
-            querySearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            querySearchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            querySearchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            querySearchBar.heightAnchor.constraint(equalToConstant: 40),
-       ])
+            completion()
+        }
+        
+        
+        //    func updateUI(with categories: [Recipe]) {
+        //        recipes.append(contentsOf: categories)
+        //
+        //        DispatchQueue.main.async {
+        //            self.collectionView.reloadData()
+        //            //self.view.bringSubviewToFront(self.tableView)
+        //        }
+        //    }
+        
+        
+        func layoutUI() {
+            querySearchBar.delegate = self
+            
+            NSLayoutConstraint.activate([
+                querySearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+                querySearchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+                querySearchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+                querySearchBar.heightAnchor.constraint(equalToConstant: 40),
+            ])
+        }
     }
-}
-
-extension BookmarksVC: UISearchBarDelegate {
+    
+    extension BookmarksVC: UISearchBarDelegate {
         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             querySearchBar.resignFirstResponder()
         }
     }
-
-extension BookmarksVC {
-    func configureCompositionalLayout() {
-        let layout = UICollectionViewCompositionalLayout {sectionIndex,enviroment in
-            switch sectionIndex {
-            case 0 :
-                return UIHelper.rvSection()
-                
-            case 1 :
-                return UIHelper.madeItSection()
-                
-            case 2 :
-                return UIHelper.breakfastSection()
-                
-            case 3 :
-                return UIHelper.lunchSection()
-                
-            case 4 :
-                return UIHelper.dinnerSection()
-                
-            default:
-                return UIHelper.categoriesSection()
+    
+    extension BookmarksVC {
+        func configureCompositionalLayout() {
+            let layout = UICollectionViewCompositionalLayout {sectionIndex,enviroment in
+                switch sectionIndex {
+                case 0 :
+                    return UIHelper.rvSection()
+                    
+                case 1 :
+                    return UIHelper.madeItSection()
+                    
+                case 2 :
+                    return UIHelper.breakfastSection()
+                    
+                case 3 :
+                    return UIHelper.lunchSection()
+                    
+                case 4 :
+                    return UIHelper.dinnerSection()
+                    
+                default:
+                    return UIHelper.categoriesSection()
+                }
             }
+            collectionView.setCollectionViewLayout(layout, animated: true)
         }
-        collectionView.setCollectionViewLayout(layout, animated: true)
     }
-}
+
